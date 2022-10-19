8fdec4d77f0fd29daebef5fc05411b725fbc8674
surajgavali <suraj.gavali@bajajfinserv.in>
2022-10-19 10:55:02 +0530

    Initial Commit

---
+++ WalMart/src/main/java/com/example/walmart/WalMartApplication.java
@@ -0,0 +1,13 @@
+package com.example.walmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalMartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalMartApplication.class, args);
+    }
+
+}
