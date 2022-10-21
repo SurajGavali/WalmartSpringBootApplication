c7273748604a2fce66f30e60028773b10dad20ae
surajgavali <suraj.gavali@bajajfinserv.in>
2022-10-21 15:50:57 +0530

    removed @Query part

--- WalMart/src/main/java/com/example/walmart/db/userJPAdb.java
+++ WalMart/src/main/java/com/example/walmart/db/userJPAdb.java
@@ -2,6 +2,7 @@ package com.example.walmart.db;
 
 import com.example.walmart.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +14,4 @@ public interface userJPAdb extends JpaRepository<User,Integer> {
     User findUserByuserName(String userName);
 
     List<User> findByOrderByUserNameDesc();
-
-
-
 }
